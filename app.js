34e31271fa0877c7651e3f6886484965efbdf282
antocappellari <antonella.cappellari@outlook.com>
2023-01-20 17:28:55 -0300

    render

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT||3030
 
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
-app.listen(3030,()=>{
-    console.log("Servidor corriendo en el puerto 3000")
-});
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
@@ -17,3 +15,5 @@ app.get('/register', (req,res)=>{
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
+
+app.listen(PORT,()=>{console.log(`Servidor corriendo en el puerto ${PORT}`)});
